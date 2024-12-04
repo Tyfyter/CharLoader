7cc079d2c615b595ae2a78ba99811d1c19892c0c
Tyfyter <tyfyter2616@gmail.com>
2024-12-03 20:08:35 -0700

    now with 100% less DynamicMethod

--- CharLoader.cs
+++ CharLoader.cs
@@ -7,8 +7,13 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using Terraria;
+using Terraria.Chat;
 using Terraria.GameContent;
+using Terraria.Map;
 using Terraria.ModLoader;
+using Terraria.UI.Chat;
+using Terraria.ID;
+using System.Text;
 
 namespace CharLoader {
 	public class CharLoader : Mod {
@@ -17,52 +22,20 @@ namespace CharLoader {
 		public int CharCount(DynamicSpriteFont font) => charCount.TryGetValue(font, out int value) ? value : 0;
 		Dictionary<DynamicSpriteFont, Dictionary<string, char>> namedChars;
 		public override void Load() {
-			charCount = new();
-			namedChars = new();
-			FieldInfo spriteCharacters = typeof(DynamicSpriteFont).GetField("_spriteCharacters", BindingFlags.NonPublic | BindingFlags.Instance);
-			Type SpriteCharacterData = typeof(DynamicSpriteFont).GetNestedType("SpriteCharacterData", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-			Type dict = typeof(Dictionary<,>).MakeGenericType(typeof(char), SpriteCharacterData);
-			MethodInfo set = dict.GetMethod("set_Item");
-			ConstructorInfo ctor = SpriteCharacterData.GetConstructor(new Type[] {
-				typeof(Texture2D),
-				typeof(Rectangle),
-				typeof(Rectangle),
-				typeof(Vector3)
-			});
-			DynamicMethod getterMethod = new DynamicMethod("AddCharacter", typeof(void), new Type[] {
-				typeof(DynamicSpriteFont),
-				typeof(char),
-				typeof(Texture2D),
-				typeof(Rectangle),
-				typeof(Rectangle),
-				typeof(Vector3)
-			}, true);
-			ILGenerator gen = getterMethod.GetILGenerator();
-
-			gen.Emit(OpCodes.Ldarg_0);
-			gen.Emit(OpCodes.Ldfld, spriteCharacters);
-			gen.Emit(OpCodes.Ldarg_1);
-			gen.Emit(OpCodes.Ldarg_2);
-			gen.Emit(OpCodes.Ldarg_3);
-			gen.Emit(OpCodes.Ldarg_S, (byte)4);
-			gen.Emit(OpCodes.Ldarg_S, (byte)5);
-			gen.Emit(OpCodes.Newobj, ctor);
-			gen.Emit(OpCodes.Call, set);
-			gen.Emit(OpCodes.Ret);
-
-			_SetCharacter = getterMethod.CreateDelegate<SetCharFunc>();
+			charCount = [];
+			namedChars = [];
 		}
 		public override void Unload() {
 			charCount = null;
 			namedChars = null;
 		}
 		static char GetNextIndex(DynamicSpriteFont font) {
-			var charCount = Instance.charCount;
-			if (!charCount.ContainsKey(font)) charCount.Add(font, 0);
+			Dictionary<DynamicSpriteFont, int> charCount = Instance.charCount;
+			charCount.TryAdd(font, 0);
 			return (char)('\uE000' + charCount[font]++);
 		}
 		public static Dictionary<string, char> GetNamedCharSet(DynamicSpriteFont font) {
-			if (!Instance.namedChars.TryGetValue(font, out var namedChars)) Instance.namedChars[font] = namedChars = new();
+			if (!Instance.namedChars.TryGetValue(font, out Dictionary<string, char> namedChars)) Instance.namedChars[font] = namedChars = [];
 			return namedChars;
 		}
 		public static char? GetNamedChar(DynamicSpriteFont font, string name) {
@@ -78,7 +51,7 @@ namespace CharLoader {
 
 				case "SetCharacter":
 				case "SetChar": {
-					SetCharacter((DynamicSpriteFont)args[1], (char)args[2], (Texture2D)args[3], (Rectangle)args[4], (Rectangle)args[5], (Vector3)args[6]);
+					((DynamicSpriteFont)args[1]).SpriteCharacters[(char)args[2]] = new((Texture2D)args[3], (Rectangle)args[4], (Rectangle)args[5], (Vector3)args[6]);
 					return null;
 				}
 
@@ -89,14 +62,9 @@ namespace CharLoader {
 			}
 			throw new ArgumentException($"{args[0]} is not a valid function, you probably have the wrong mod, this is {DisplayName} and all of its functions are listed in its description");
 		}
-		public delegate void SetCharFunc(DynamicSpriteFont font, char index, Texture2D texture, Rectangle glyph, Rectangle padding, Vector3 kerning);
-		SetCharFunc _SetCharacter;
-		public static void SetCharacter(DynamicSpriteFont font, char index, Texture2D texture, Rectangle glyph, Rectangle padding, Vector3 kerning) {
-			Instance._SetCharacter(font, index, texture, glyph, padding, kerning);
-		}
 		public static char AddCharacter(DynamicSpriteFont font, Texture2D texture, Rectangle glyph, Rectangle padding, Vector3 kerning, string name = null) {
 			char index = GetNextIndex(font);
-			SetCharacter(font, index, texture, glyph, padding, kerning);
+			font.SpriteCharacters[index] = new(texture, glyph, padding, kerning);
 			if (name is not null) GetNamedCharSet(font)[name] = index;
 			return index;
 		}
@@ -105,21 +73,30 @@ namespace CharLoader {
 		public override string Command => "showchar";
 		public override CommandType Type => CommandType.Chat;
 		public override void Action(CommandCaller caller, string input, string[] args) {
-			string character = null;
+			StringBuilder characters = new();
 			string error = null;
-			if (CharLoader.GetNamedChar(FontAssets.MouseText.Value, args[0]) is char _char) {
-				character = _char.ToString();
-			} else if (int.TryParse(args[0], out int index) || TryParseHex(args[0], out index)) {
-				if (index < 0) error = "Negative indecies are not allowed";
-				else if (index > 0x18FF) error = "The PUA I'm using is U+E000–U+F8FF, so a maximum index of 18FF (6399) is allowed";
-				character = ((char)('\uE000' + index)).ToString();
-			} else {
-				error = $"no character found with name {args[0]}";
+			for (int i = 0; i < args.Length; i++) {
+				if (CharLoader.GetNamedChar(FontAssets.MouseText.Value, args[i]) is char _char) {
+					characters.Append(_char);
+				} else if ((args[i].StartsWith("0x") && TryParseHex(args[i][2..], out int index)) || int.TryParse(args[i], out index)) {
+					if (index < 0) error = "Negative indices are not allowed";
+					else if (index > 0x18FF) error = "The PUA this mod uses is U+E000–U+F8FF, so a maximum index of 18FF (6399) is allowed";
+					characters.Append((char)('\uE000' + index));
+				} else if (string.IsNullOrWhiteSpace(args[i])) {
+					characters.Append(' ');
+				} else {
+					error = $"no character found with name {args[i]}";
+				}
 			}
 			if (error is not null) {
-				Main.NewText(error, Color.Red);
+				caller.Reply(error, Color.Red);
 			} else {
-				Main.NewText(character);
+				ChatMessage message = ChatManager.Commands.CreateOutgoingMessage(characters.ToString());
+				if (Main.netMode == NetmodeID.MultiplayerClient) {
+					ChatHelper.SendChatMessageFromClient(message);
+				} else if (Main.netMode == NetmodeID.SinglePlayer) {
+					ChatManager.Commands.ProcessIncomingMessage(message, Main.myPlayer);
+				}
 			}
 		}
 		static bool TryParseHex(string input, out int value) {
@@ -131,5 +108,11 @@ namespace CharLoader {
 				return false;
 			}
 		}
+	}
+	public class CharCommand : ShowCharCommand {
+		public override string Command => "char";
+	}
+	public class CharsCommand : ShowCharCommand {
+		public override string Command => "chars";
 	}
 }
